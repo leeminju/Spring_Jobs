f9cb2cb7a33815b26cdcbd6a2668cdffd1e852a0
leeminju <leemj4090@naver.com>
2023-11-21 16:16:58 +0900

    init commit

---
+++ src/main/java/com/example/spring_jobs/SpringJobsApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJobsApplication.class, args);
+    }
+
+}
